4207fdc143c3ea61d52da34b4e9239db0506c135
startofonepoint <startofonpoint@gmail.com>
2015-03-17 19:45:16 +0900

    Initial Commit

---
+++ MapViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapViewTest
+//
+//  Created by lostin1 on 2015. 3. 17..
+//  Copyright (c) 2015년 lostin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
